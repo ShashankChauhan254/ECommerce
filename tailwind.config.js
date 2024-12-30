b0894b3849f8c3e5eae028fae5319bef03e2569e
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-30 23:30:39 +0530

    30/12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
+import DarkMode from './src/Components/Navbar/darkMode.jsx';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: [],
   theme: {
     extend: {
       colors: {
-        primary: "#4263eb",
-        secondary: "#63e6be",
+        'primary': '#4263eb',
+        'secondary': '#63e6be',
       },
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          sm: "3rem",
-        },
+          DEFAULT: '1rem',
+          sm: '3rem',
+        }
       },
     },
   },
+  DarkMode: "selector",
   plugins: [],
-};
+}
+
