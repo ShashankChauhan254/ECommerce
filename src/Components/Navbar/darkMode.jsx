30dcebf1f5573ac8eac800188d6a091e5cd1ca2a
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-30 23:14:25 +0530

    30/12

--- src/Components/Navbar/darkMode.jsx
+++ src/Components/Navbar/darkMode.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LightButton from '../../assets/website/liglight-mode-button.png';
+import LightButton from '../../assets/website/light-mode-button.png';
 import DarkButton from '../../assets/website/dark-mode-button.png';
 
 const DarkMode = () => {
