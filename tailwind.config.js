87dbae43390f17e2049e8685b0604a693f936c1a
ShashankChauhan254 <chauhan.19@iitj.ac.in>
2025-01-01 21:48:39 +0530

    01/01

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ import DarkMode from './src/Components/Navbar/darkMode';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
